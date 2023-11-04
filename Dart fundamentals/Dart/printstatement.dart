5eb6e998366d3e9723914fc84bb8f8318ee53d3b
Archana1825 <archanap1825@gmail.com>
2023-11-04 15:25:56 +0530

    Print statements

---
+++ Dart fundamentals/Dart/printstatement.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  //printing string 
+  print("Hello");
+
+  //printing initiger
+  print( 5);
+
+  //priniting a variable using string interpolation 
+  int num1 =5;
+  print("The number is: $num1");
+
+  //concatenating string
+  String name ="Archana";
+  print('Name:' + name);
+
+  int a=5 , b =6;
+  print( a-b);
+  print ('sum of $a and $b is ${a+b}'); //printing multiple values
+ 
+
+}
