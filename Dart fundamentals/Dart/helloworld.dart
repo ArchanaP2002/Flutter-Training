d5c6d583aa0be5ae16d1a4ef433210f94debc2c8
Archana1825 <archanap1825@gmail.com>
2023-11-04 15:02:01 +0530

    Printing hello world in Console

---
+++ Dart fundamentals/Dart/helloworld.dart
@@ -0,0 +1,5 @@
+void main(){ //main method is the entry point of dart/application
+  print("Hello World!"); //string
+  print(4-2); //each statement or line should be end with ;
+  print(false); // Boolean
+}
