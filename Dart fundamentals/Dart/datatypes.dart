889781e40284ab283cf9f39da1fa6b2bb702a998
Archana1825 <archanap1825@gmail.com>
2023-11-06 16:01:30 +0530

    datatypes and variables

--- Dart fundamentals/Dart/datatypes.dart
+++ Dart fundamentals/Dart/datatypes.dart
@@ -1,1 +1,38 @@
 //no float and long data types in number
+
+//data types :
+/*
+1. numbers  - int and double
+2. string
+3. boolean
+4. list
+5. map
+6. runes
+7. sets
+8. null
+ */
+
+void main() {
+ //Numbers
+  int n1 = 5;
+  double n2 = 6.44;
+  print('$n1 and $n2');
+  print(n2.floor());
+  print(n2.toString());
+  print(n2.toStringAsFixed(1));
+
+  // string
+  String name = '1';
+  print(name.runtimeType);
+  print(name.length);
+  print(name.runes);
+  //int name1 = int.parse(name);
+  int intvalue = int.parse(name);
+  print(intvalue.runtimeType);
+
+ // boolean
+  bool istrue = true;
+  print(istrue);
+
+ //list
+}
--- Dart fundamentals/Dart/variables.dart
+++ Dart fundamentals/Dart/variables.dart
@@ -1,38 +1,40 @@
-
-int a=10; //global variable
-void main(){
-  var name = "archana"; //declaring using var  //local variable 
+int a = 10; //global variable
+void main() {
+  var name = "archana"; //declaring using var  //local variable
   var age = 21;
   print('name: $name , age: $age');
 
-  age= 22;  //Reassigning a variable
-  print( "Updated Age: $age");
+  age = 22; //Reassigning a variable
+  print("Updated Age: $age");
 
   //explicit declaration
   int num = 30;
   String message = "Hello All!";
-  print( 'num: $num & message: $message');
+  print('num: $num & message: $message');
 
   // Constant declaration
   const double piValue = 3.14159;
   print('The value of pi is $piValue');
 
-  
   // Declaring Variables
-double prize = 1130.2232323233233; // valid.
-print(prize.toStringAsFixed(2));
+  double prize = 1130.2232323233233; // valid.
+  print(prize.toStringAsFixed(2));
 
-double amount = 12.344;
-print(amount.round());  //round the given value
+  double amount = 12.344;
+  print(amount.round()); //round the given value
 
-double amount1 = 12.344;
-print(amount1.toStringAsFixed(1));
+  double amount1 = 12.344;
+  print(amount1.toStringAsFixed(1));
 
-String multiLine = '''
-hello,
-hi
-''';
+  //printing multi line
+  String multiLine = '''  
+  hello,
+  hi
+  ''';
+  print(multiLine);
 
-print(multiLine);
+  //next line and tab
 
+  print(' hello \n kanini'); // \n for next line
+  print('kanini\torganization'); // \t giving space
 }
