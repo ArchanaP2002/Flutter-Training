9aec4cd67549fae54e6f52c19c91995898664d8d
Archana1825 <archanap1825@gmail.com>
2023-11-25 12:30:12 +0530

    ternary Operator

---
+++ Dart fundamentals/Dart/ternaryOperator.dart
@@ -0,0 +1,16 @@
+void main (){
+  //syntax 
+  // condition ? if true : if not true
+
+  //-------------------------------
+  // instead of using if else we can use ternary operator
+  int age = 20;
+  var valid = (age >= 18) ? 'voter' : 'Not Voter' ;
+  print("You are $valid");
+
+  int days = 20;
+   (days >= 18) ? print("You are above 18") :  print("You are under 18") ;
+
+
+
+}
