3a956908ece657e0fbcd6cfcfe2b6f3f9f761fc0
Archana1825 <archanap1825@gmail.com>
2023-11-26 10:34:20 +0530

    String Function in Dart

---
+++ Dart fundamentals/Dart/stringfunction.dart
@@ -0,0 +1,8 @@
+void main(){
+  String title = "Hello All!";
+  print(title.length);  // print the total length 
+  print(title.startsWith("Hello"));
+  print(title.substring(2,8));
+  print(title.isNotEmpty);
+  print(title.hashCode);
+}
